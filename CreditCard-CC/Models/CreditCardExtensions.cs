ebb3a6b69a1f4cc58b218588195bed3c165c8529
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-07-21 18:14:49 +0000

    add model extensions

---
+++ CreditCard-CC/Models/CreditCardExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace CreditCard_CC.Models
+{
+    public static class CreditCardExtensions
+    {
+        public static bool IsValidCard(this CreditCard creditCard)
+        {
+            creditCard.Number = creditCard.Number.Replace(" ", ""); // Remove any spaces
+
+            if (!IsNumeric(creditCard.Number))
+                return false;
+
+            int sum = 0;
+
+            for (int i = creditCard.Number.Length - 1; i >= 0; i -= 2)
+            {
+                int digit = int.Parse(creditCard.Number[i].ToString());
+
+                digit *= 2;
+                if (digit > 9)
+                    digit -= 9;
+
+                sum += digit;
+            }
+
+            return sum % 10 == 0;
+        }
+
+        private static bool IsNumeric(string value)
+        {
+            var charArray = value.ToList();
+            return charArray.All(x => x > '9' || x < '0');
+        }
+    }
+}
