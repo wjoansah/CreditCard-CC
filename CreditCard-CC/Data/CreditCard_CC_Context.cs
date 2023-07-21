ddf7a36dc3e345490ab5089ce6e72e1dbcd8b979
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-07-21 18:14:19 +0000

    add db context

---
+++ CreditCard-CC/Data/CreditCard_CC_Context.cs
@@ -0,0 +1,17 @@
+﻿using CreditCard_CC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditCard_CC.Data
+{
+    public class CreditCard_CC_Context : DbContext
+    {
+        public DbSet<CreditCard> CreditCards { get; set; }
+
+        public CreditCard_CC_Context() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("CreditCardsDb");
+        }
+    }
+}
